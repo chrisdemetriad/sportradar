5bb13d5c44b72d36ba1724171092d9528956e51d
Chris <chris.demetriad@thecommercepartnership.com>
2024-11-05 20:27:50 +0000

    updating test suite with some edge cases

--- tests/scoreboard.test.js
+++ tests/scoreboard.test.js
@@ -32,10 +32,28 @@ describe("Scoreboard", () => {
 		expect(summary).toEqual(["Spain 10 - Brazil 2", "Mexico 0 - Canada 5"]);
 	});
 
-	it("should not start duplicate matches", () => {
+	it("shouldn't be able to start duplicate matches", () => {
 		const scoreboard = new Scoreboard();
 		scoreboard.startMatch("Mexico", "Canada");
 		scoreboard.startMatch("Mexico", "Canada");
 		expect(scoreboard.matches.length).toBe(1);
+	});
+
+	it("shouldn't be able to update a non-existent match", () => {
+		const scoreboard = new Scoreboard();
+		scoreboard.updateScore("Mexico", "Canada", 0, 5);
+		expect(scoreboard.matches.length).toBe(0);
+	});
+
+	it("shouldn't be able to finish a non-existent match", () => {
+		const scoreboard = new Scoreboard();
+		scoreboard.finishMatch("Mexico", "Canada");
+		expect(scoreboard.matches.length).toBe(0);
+	});
+
+	it("should return an empty summary when there aren't any matches", () => {
+		const scoreboard = new Scoreboard();
+		const summary = scoreboard.getSummary();
+		expect(summary).toEqual([]);
 	});
 });
