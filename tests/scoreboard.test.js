6455c0cedfc8e82e9c7d77e23e37d09d6b67d145
Chris <chris.demetriad@thecommercepartnership.com>
2024-11-05 18:45:06 +0000

    updating test suite

--- tests/scoreboard.test.js
+++ tests/scoreboard.test.js
@@ -6,4 +6,12 @@ describe("Scoreboard", () => {
 		scoreboard.startMatch("Mexico", "Canada");
 		expect(scoreboard.matches.length).toBe(1);
 	});
+
+	it("should update score", () => {
+		const scoreboard = new Scoreboard();
+		scoreboard.startMatch("Mexico", "Canada");
+		scoreboard.updateScore("Mexico", "Canada", 0, 5);
+		expect(scoreboard.matches[0].homeScore).toBe(0);
+		expect(scoreboard.matches[0].awayScore).toBe(5);
+	});
 });
